b9946be2d6f14c53acab7f1ded8f1f18cf4fdb91
Furqan Haroon <furqan@isoftstudios.com>
2022-11-17 16:03:31 +0500

    login with username or email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,17 @@ class SessionsController < Devise::SessionsController
   respond_to :html
   append_view_path 'app/views/devise'
 
+  def create
+    @user = User.find_by_email(params[:user][:email])
+    @user = User.find_by_username(params[:user][:username])
+    
+    if @user && @user.valid_password?(params[:user][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Successfully Logged In #{ @user.username }"
+    end
+      redirect_to root_url
+  end
+
   def after_sign_out_path_for(*)
     new_user_session_path
   end
